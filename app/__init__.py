01494756762c8b1f132c894d8e72b96a791b929a
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-05-02 22:29:40 +0200

    fix jsonencodeddict issue

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask, request, render_template, redirect, url_for, make_respo
 import os
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
