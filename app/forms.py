7fae1c4b4cb31e5a7c0e7d23e2e88de8bfe90e6e
Wouter Haverals <wouterhaverals@msn.com>
2018-10-21 23:02:52 +0200

    forms updated with new registration info

--- app/forms.py
+++ app/forms.py
@@ -1,25 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
-    TextAreaField
+    TextAreaField, SelectField, RadioField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
     Length
 from app.models import User, JSONEncodedDict, Annotation
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Gebruikersnaam', validators=[DataRequired()])
+    password = PasswordField('Wachtwoord', validators=[DataRequired()])
+    remember_me = BooleanField('Onthoud mij')
+    submit = SubmitField('Aanmelden')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Gebruikersnaam', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Wachtwoord', validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+        'Herhaal wachtwoord', validators=[DataRequired(), EqualTo('password')])
+    language = RadioField('Moedertaal', choices = [('vlaams', 'Vlaams'), 
+      ('nederlands', 'Nederlands')])
+    middledutch = RadioField('Hoe schat u uw kennis in van het Middelnederlands?', choices = [('geen', 'Geen'), 
+      ('gemiddeld', 'Gemiddeld'), ('goed', 'Goed')])
+    submit = SubmitField('Registreer')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
