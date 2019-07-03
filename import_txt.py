b2e09187afdc8cc901e7e521de8570afb4fb592f
jpe <jef.peeraer@uantwerpen.be>
2019-07-03 15:55:09 +0200

    added extra class Report to models.py, for simple reporting functionality moved column frag_nbr from syllable to fragment

--- import_txt.py
+++ import_txt.py
@@ -7,66 +7,71 @@ import sqlite3 as lite
 # story ,and are ordered as lists of lists
 # where each word is a list, containing zero or more syllables
 #
-# this script populates the 'story' table as well as 
+# this script populates the 'story' table as well as
 # the 'syllable' table.
 # remark : doesn't work on updates yet!!!!!!!
 #
 def import_txt_files(path, cur):
-	# all data files are in the subdir data
-	files = glob.glob(path + '*.txt')
-	if len(files) > 0:
-		print(" files found ", len(files))
-		for file in files:
-			print("-------> processing file ", file)
-			header = True
-			alist = []
-			line_nbr = 0 # because of the header :-)
-			word_nbr = 1 
-			syl_nbr = 1
-			with open(file, 'r') as f:
-				m = re.match(r'.*\/(\w+)_(\d+)\.txt', file)
-				if m:
-					story = m.group(1)
-					fragment = m.group(2)
-				else:
-					print("filename does not match")
-				for line in f:
-					line_wnl = line.rstrip()
-					if header:
-						print("story -> ", story)
-						print("descr -> ", line_wnl)
-						header = False
-						cur.execute("select * from story where title = ? ", (story, ))
-						st = cur.fetchone()
-						if st is None:
-							cur.execute("insert into story (title, description) values( ?, ?)", (story, line_wnl))
-							story_id = cur.lastrowid
-						else:
-							story_id = st[0]
-						#we have a new fragment
-						cur.execute("insert into fragment (story_id) values ( ? )", (story_id,))
-						frag_id = cur.lastrowid						
-					else:
-						print("line -> ", line_wnl)
-						llist = eval(line_wnl)
-						# llist is a list of lists 
-						for alist in llist:
-							for syl in alist:
-								print("frag_id %s, syllable_nr %s, word_nbr %s, line_nbr %s, fragment %s , %s"  \
-								% (frag_id, syl_nbr, word_nbr, line_nbr, fragment, syl))
-								cur.execute("insert into syllable (frag_id, frag_nbr, line_nbr, word_nbr, syll_nbr, syllable ) \
-								values ( ?, ?, ?, ? ,? , ? )", (frag_id, fragment, line_nbr, word_nbr, syl_nbr, syl))
-								syl_nbr += 1
-							syl_nbr = 1
-							word_nbr += 1
-						word_nbr = 1
-					line_nbr += 1
-	else:
-		print("no files found in this path %s" % (path))
-    	
+    # all data files are in the subdir data
+    files = glob.glob(path + '*.txt')
+    if len(files) > 0:
+        print(" files found ", len(files))
+        for file in files:
+            print("-------> processing file ", file)
+            header = True
+            alist = []
+            line_nbr = 0 # because of the header :-)
+            word_nbr = 1
+            syl_nbr = 1
+            with open(file, 'r') as f:
+                m = re.match(r'.*\/(\w+)_(\d+)\.txt', file)
+                if m:
+                    story = m.group(1)
+                    fragment_nbr = m.group(2)
+                else:
+                    print("filename does not match")
+                for line in f:
+                    line_wnl = line.rstrip()
+                    if header:
+                        print("story -> ", story)
+                        print("descr -> ", line_wnl)
+                        header = False
+                        cur.execute("select * from story where title = ? ", (story, ))
+                        st = cur.fetchone()
+                        if st is None:
+                            print("no story found with title %s, inserting a new one" % story)
+                            cur.execute("insert into story (title, description) values( ?, ?)",
+                                        (story, line_wnl))
+                            story_id = cur.lastrowid
+                        else:
+                            story_id = st[0]
+                        #we have a new fragment
+                        cur.execute("insert into fragment (story_id, frag_nbr) values ( ?, ? )",
+                                    (story_id, fragment_nbr))
+                        frag_id = cur.lastrowid
+                        print("frag_id -> %d", frag_id)
+                    else:
+                        print("line -> ", line_wnl)
+                        llist = eval(line_wnl)
+                        # llist is a list of lists
+                        for alist in llist:
+                            for syl in alist:
+                                print("frag_id %s, syllable_nr %s, word_nbr %s, line_nbr %s, %s"  \
+                                      % (frag_id, syl_nbr, word_nbr, line_nbr, syl))
+                                cur.execute("insert into syllable (frag_id, line_nbr, word_nbr, syll_nbr, \
+                                syllable ) values ( ?, ?, ? ,? , ? )", \
+                                            (frag_id, line_nbr, word_nbr, syl_nbr, syl))
+                                syl_nbr += 1
+                            syl_nbr = 1
+                            word_nbr += 1
+                        word_nbr = 1
+                    line_nbr += 1
+    else:
+        print("no files found in this path %s" % (path))
+                
 #f __name__ == 'main':
 print("ok, starting conversion txt-> db")
-path='./data/'
+path = './data/'
 con = lite.connect('app.db')
 cur = con.cursor()
 import_txt_files(path, cur)
--- scanblog.py
+++ scanblog.py
@@ -1,2 +1,7 @@
 from app import app, db
-from app.models import User, Annotation, Story, Syllable, Fragment, Scanned
+from app.models import User, Annotation, Story, Syllable, Fragment, Scanned, Report
+import pprint
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Annotation=Annotation, Story=Story, Fragment=Fragment, Scanned=Scanned, Syllable=Syllable, Report=Report)
